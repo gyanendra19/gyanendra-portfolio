2e0cbb48993b7cdcbf1a95735dd07bd9d27d9dc6
gyanendra19 <gyanendrav416@gmail.com>
2025-04-04 02:38:08 +0530

    changes

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='w-full px-4 py-2 flex md:gap-2 md:flex-row flex-col mt-3 md:mt-0 tracking-wide justify-center items-center bg-white/20 text-sm'>
       <p>© Gyanendra Verma |</p>
-      <p>Made with love and hands❣️</p>
+      <p>Made with love❣️</p>
       </div>
   )
 }
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,35 +1,83 @@
-import React, { useState } from 'react'
-import ProjectImage from './projectEach'
-import { RiExternalLinkFill } from '@remixicon/react'
+import React, { useState } from "react";
+import ProjectImage from "./projectEach";
+import { RiExternalLinkFill } from "@remixicon/react";
 
 const Projects = () => {
-    const [project, setProject] = useState('fitness')
+  const [project, setProject] = useState("fitness");
 
-    const prolectLinks = project === 'fitness' ? ['https://github.com/gyanendra19/everydayfit', 'https://everydayfit.vercel.app'] :
-    project === 'form' ? ['https://github.com/gyanendra19/aeonaxyfirst', 'https://aeonaxyfirst.netlify.app'] :
-    project === 'dribble' ? ['https://github.com/gyanendra19/aeonaxySecond', 'https://aeonaxy-second.vercel.app'] :
-    ['https://github.com/gyanendra19/climate-crafter', 'https://climate-crafter.vercel.app']
+  const prolectLinks =
+    project === "fitness"
+      ? [
+          "https://github.com/gyanendra19/everydayfit",
+          "https://everydayfit.vercel.app",
+        ]
+      : project === "form"
+      ? [
+          "https://github.com/gyanendra19/aeonaxyfirst",
+          "https://aeonaxyfirst.netlify.app",
+        ]
+      : project === "dribble"
+      ? [
+          "https://github.com/gyanendra19/aeonaxySecond",
+          "https://aeonaxy-second.vercel.app",
+        ]
+      : [
+          "https://github.com/gyanendra19/climate-crafter",
+          "https://climate-crafter.vercel.app",
+        ];
 
-    return (
-        <>
-            <div className='absolute w-44 h-32 bg-red-400 z-[1] top-10 md:left-64 blur-[240px]'></div>
-            <div className='flex flex-wrap justify-center gap-6 whitespace-nowrap pb-4 text-black md:text-lg text-sm font-medium tracking-wider relative z-[2]'>
-                <p onClick={() => setProject('fitness')} className='px-4 cursor-pointer py-1 bg-white rounded-sm'>Fitness app</p>
-                <p onClick={() => setProject('form')} className='px-4 cursor-pointer py-1 bg-white rounded-sm'>Multi-step Form</p>
-                <p onClick={() => setProject('dribble')} className='px-4 cursor-pointer py-1 bg-white rounded-sm'>Dribble sign-up form</p>
-                <p onClick={() => setProject('weather')} className='px-4 cursor-pointer py-1 bg-white rounded-sm'>Live weather</p>
-            </div>
-            <div className='md:w-[93%]'>
-                <div className='w-full h-full flex gap-10 pl-2 project overflow-x-scroll border-t border-gray-200'>
-                   <ProjectImage project = {project} />
-                </div>
-                <div className='flex w-full mt-6 md:mt-0 justify-center gap-8' >
-                    <a href={`${prolectLinks[0]}`} target='_blank' className='text-gray-300 flex gap-2 items-center cursor-pointer text-xl tracking-widest'>Github <RiExternalLinkFill size={16} /></a>
-                    <a href={`${prolectLinks[1]}`} target='_blank' className='text-gray-300 items-center flex gap-2 cursor-pointer text-xl tracking-widest'>Live <RiExternalLinkFill size={16} /></a>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="absolute w-44 h-32 bg-red-400 z-[1] top-10 md:left-64 blur-[240px]"></div>
+      <div className="flex flex-wrap justify-center gap-6 whitespace-nowrap pb-4 text-black md:text-lg text-sm font-medium tracking-wider relative z-[2]">
+        <p
+          onClick={() => setProject("fitness")}
+          className="px-4 cursor-pointer py-1 bg-white rounded-sm"
+        >
+          Fitness app
+        </p>
+        <p
+          onClick={() => setProject("form")}
+          className="px-4 cursor-pointer py-1 bg-white rounded-sm"
+        >
+          Multi-step Form
+        </p>
+        <p
+          onClick={() => setProject("dribble")}
+          className="px-4 cursor-pointer py-1 bg-white rounded-sm"
+        >
+          Dribble sign-up form
+        </p>
+        <p
+          onClick={() => setProject("weather")}
+          className="px-4 cursor-pointer py-1 bg-white rounded-sm"
+        >
+          Live weather
+        </p>
+      </div>
+      <div className="md:w-[93%]">
+        <div className="h-full flex gap-10 pl-2 overflow-x-scroll border-t border-gray-200">
+            <ProjectImage project={project} />
+        </div>
+        <div className="flex w-full mt-6 md:mt-4 justify-center gap-8">
+          <a
+            href={`${prolectLinks[0]}`}
+            target="_blank"
+            className="text-gray-300 flex gap-2 items-center cursor-pointer text-xl tracking-widest"
+          >
+            Github <RiExternalLinkFill size={16} />
+          </a>
+          <a
+            href={`${prolectLinks[1]}`}
+            target="_blank"
+            className="text-gray-300 items-center flex gap-2 cursor-pointer text-xl tracking-widest"
+          >
+            Live <RiExternalLinkFill size={16} />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
